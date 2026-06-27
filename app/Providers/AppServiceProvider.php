fcd1f52cb98d69b839e3461b30443e328407c784
j09392w-source <j09392w@gmail.com>
2026-06-26 20:37:28 -0600

    Forzar enlaces HTTPS para cargar el CSS

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -3,6 +3,7 @@
 namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
+use Illuminate\Support\Facades\URL; // <-- 1. Agrega esta línea
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -19,6 +20,9 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-        //
+        // 2. Obligamos a Laravel a usar HTTPS en el entorno de producción
+        if (env('APP_ENV') === 'production') {
+            URL::forceScheme('https');
+        }
     }
 }
